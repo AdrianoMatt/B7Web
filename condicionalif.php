72bdf32de8ed79c7166b0cc633ae3de7e8b3785f
Adriano Afonso Matt <adriano.matt@gpb.local>
2022-10-13 17:29:48 -0300

    Novo Curso v6

---
+++ condicionalif.php
@@ -0,0 +1,11 @@
+<?php
+$idade = 18;
+#if ($idade >= 18) {
+#    echo "Maior de idade";
+#}
+#else { 
+#    echo "Menor de idade";
+#};
+
+#Outra forma de realizar usando condicional ternária
+echo ($idade <18) ? 'Menor de idade' : 'Maior de idade';
