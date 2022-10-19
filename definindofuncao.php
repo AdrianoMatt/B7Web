180ea3f2dd458d96edc03f864cb08848806a8418
Adriano Afonso Matt <adriano.matt@gpb.local>
2022-10-19 17:28:48 -0300

    Novo Curso v11

---
+++ definindofuncao.php
@@ -0,0 +1,17 @@
+<?php
+function subsequente (){
+    for ($q=0; $q<10; $q++){
+        echo $q. "</br>";
+    }
+    echo '<hr/>';
+}
+subsequente();
+subsequente();
+
+function somar($n1, $n2){
+    $total = $n1 + $n2;
+    return $total;
+
+}
+$soma = somar(10, 5);
+echo "TOTAL: ". $soma;
--- exercicioloop2.php
+++ exercicioloop2.php
@@ -1,11 +1,16 @@
 <?php
 $j = '';
-for ($i = 1; $i<=20; $i++){
-    #for($j=0; $j<20; $j++){
+for ($i = 0; $i<=20; $i++){
         echo $j . '</br>';
-        echo $j.='-';
-        
-        
-   # }
-    echo '</br>';
+         $j.='-';
+        ;
+}
+echo '</br>';
+
+#Outra forma que podia ser feita
+for ($i=0; $i<=20; $i++){
+    for($j=1; $j<=$i; $j++){
+        echo "-";
+    };
+    echo "<br/>";
 }
