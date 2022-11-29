941fe3ae773e5b124977ea6a906302f80f2a2cc9
Adriano Afonso Matt <adriano.matt@gpb.local>
2022-11-29 17:34:52 -0300

    Novo Curso v15

--- parametroreferenciavalor.php
+++ parametroreferenciavalor.php
@@ -1,10 +1,17 @@
 <?php
 #Aula de Parâmetro: Referência ou Valor
-function somar($n1, $n2){
+function somar($n1, $n2, &$total){
     $total = $n1 + $n2;
-    return $total;
+
 }
 $x = 3;
-$x = 2;
-$soma = somar($x, $y);
-echo "O total é: ". $soma;
+$y = 2;
+$soma = 0;
+somar($x, $y, $soma);
+echo $x. ' + ' . $y. '=' . $soma;
+
+echo "<hr>";
+//listar de forma ordenada usando sort
+$lista = [4, 9, 2];
+sort($lista);
+print_r($lista);
