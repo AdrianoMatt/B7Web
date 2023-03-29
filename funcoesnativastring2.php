e9cf46a7ba82e13bdf7b3ac5d5cbc3bf0175b434
AdrianoMatt <snoot.adriano@gmail.com>
2023-03-29 00:35:46 -0300

    Novo Curso 29/03/23

--- funcoesnativastring2.php
+++ funcoesnativastring2.php
@@ -1,7 +1,21 @@
 <?php
-$nomeCompleto = 'Adriano Matt';
-$posicao = strpos($nomeCompleto, 'a');
+#strpos serve para localizar a posição que inicia a condição 
+$nomeCompleto = 'adriano matt';
+$posicao = strpos($nomeCompleto, 'r');
 echo $posicao;
+echo "<br>";
+if($posicao>=0){
+    echo "ACHOU";
+    } else {
+echo "NÃO ACHOU";
+    }
+    echo "<br>";
+#Para deixar a primeira letra da string em maiúscula
+echo ucfirst($nomeCompleto);
+echo "<br>";
+#Para deixar todas as primeiras letras da string em maiúscula
+echo ucwords($nomeCompleto);
+
 
 
 
